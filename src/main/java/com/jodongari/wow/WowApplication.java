fce0634c7336a116bcb8e4469d594ba47a23a4c1
DESKTOP-8H087C4\jung <qaqa313a@gmail.com>
2021-06-05 14:55:30 +0900

    Feature: init setting

---
+++ src/main/java/com/jodongari/wow/WowApplication.java
@@ -0,0 +1,13 @@
+package com.jodongari.wow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WowApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jodongari/wow/WowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jodongari.wow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
