0d8ba73b6175903c754af7b6d5fc35ae7f1a2838
DESKTOP-8H087C4\jung <qaqa313a@gmail.com>
2021-06-12 17:15:10 +0900

    Fix: #3 front HomeView

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,12 +1,21 @@
 <template>
-  <div id="home">무야호</div>
+  <v-container fluid>
+    <v-main>
+      <nav-bar></nav-bar>
+      <video-card></video-card>
+    </v-main>
+  </v-container>
 </template>
 
 <script>
+import NavBar from "@/components/common/NavBar.vue";
+import VideoCard from "@/components/VideoCard.vue";
+
 export default {
   name: "Home",
-
   components: {
+    NavBar: NavBar,
+    VideoCard: VideoCard,
   },
 };
 </script>
