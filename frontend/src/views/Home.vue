ccea58beccc5114693a89d9d77e0cd29eda2e5f0
DESKTOP-8H087C4\jung <qaqa313a@gmail.com>
2021-08-07 18:11:46 +0900

    Feature: #7 connecting proxy

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -3,76 +3,14 @@
     <v-main>
       <nav-bar></nav-bar>
       <video-card></video-card>
-      <div>
-        <!-- <video
-          data-dashjs-player
-          autoplay
-          src="@/assets/video/jisu.mpd"
-          controls
-        ></video> -->
-
-        <!-- <video data-dashjs-player autoplay controls>
-          <source src="@/assets/video/jisu.mpd" type="application/dash+xml" />
-        </video> -->
-        <!-- <video data-dashjs-player autoplay controls>
-          <source
-            src="https://dash.akamaized.net/akamai/bbb_30fps/bbb_30fps.mpd"
-            type="application/dash+xml"
-          />
-        </video> -->
-        <!-- <video
-          data-dashjs-player
-          autoplay
-          src="https://dash.akamaized.net/akamai/bbb_30fps/bbb_30fps.mpd"
-          controls
-        ></video> -->
-        <!-- <video
-          id="example-video"
-          width="600"
-          height="300"
-          class="video-js vjs-default-skin"
-          controls
-        ></video> -->
-        <!-- <video data-dashjs-player autoplay controls>
-          <source
-            src="../assets/video/jisoo_dash.mpd"
-            type="application/dash+xml"
-          />
-        </video> -->
-        <!-- <video
-          id="example-video"
-          width="600"
-          height="300"
-          class="video-js vjs-default-skin"
-          controls
-        ></video> -->
-
-        <video
-          id="example-video"
-          width="960"
-          height="540"
-          class="video-js vjs-default-skin"
-          controls
-        >
-          <source
-            src="@/assets/video/test2/output.m3u8"
-            type="application/x-mpegURL"
-          />
-        </video>
-      </div>
+      <div><video id="videoPlayer" controls></video></div>
     </v-main>
   </v-container>
 </template>
-<script src="https://vjs.zencdn.net/7.11.4/video.min.js"></script>
-<script src="http://cdn.dashjs.org/v4.0.0/dash.all.min.js"></script>
 <script>
-import "/node_modules/dashjs/dist/dash.all.min.js";
-// import videojs from "video.js";
-import "dashjs";
-// import "videojs-contrib-dash";
 import NavBar from "@/components/common/NavBar.vue";
 import VideoCard from "@/components/VideoCard.vue";
-
+import dashjs from "dashjs";
 export default {
   name: "Home",
   components: {
@@ -80,23 +18,13 @@ export default {
     VideoCard: VideoCard,
   },
   mounted() {
-    var player = videojs("example-video");
-    player.play();
+    // var url = "https://dash.akamaized.net/envivio/EnvivioDash3/manifest.mpd";
+    var url = "assets/video/jisu.mpd";
+    var player = dashjs.MediaPlayer().create();
+    console.log(player);
+    console.log(document.querySelector("#videoPlayer"));
+    player.initialize(document.querySelector("#videoPlayer"), url, true);
+    console.log("확인!!");
   },
-  // methods: {},
-  // mounted: function () {
-  //   // const name = document.querySelector("video").id;
-  //   // console.log(name);
-  //   var player = videojs("example-video");
-
-  //   player.ready(function () {
-  //     player.src({
-  //       src: "assets/video/jisoo_dash.mpd",
-  //       type: "application/dash+xml",
-  //     });
-
-  //     player.play();
-  //   });
-  // },
 };
 </script>
